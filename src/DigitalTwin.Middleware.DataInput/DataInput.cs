594241d496dbbdfb71f6203e3dc3be69df1e8bb6
Thomas Niels Berthelsen (tnb) <tnb@novicell.dk>
2022-11-25 16:09:03 +0100

    update datainput

--- src/DigitalTwin.Middleware.DataInput/DataInput.cs
+++ src/DigitalTwin.Middleware.DataInput/DataInput.cs
@@ -12,11 +12,17 @@ namespace DigitalTwin.Middleware.DataInput
     {
         [JsonProperty("time")]
         public string Time => DateTime.UtcNow.ToString("yyyy-MM-ddTHH\\:mm\\:ss\\.ffffzzzz", CultureInfo.InvariantCulture);
-        [JsonProperty("output_op_pos_x")]
+        [JsonProperty("output_op_pos_x_to_op")]
         public string PosX { get; set; }
-        [JsonProperty("output_op_pos_y")]
+        [JsonProperty("output_op_pos_y_to_op")]
         public string PosY { get; set; }
-        [JsonProperty("output_op_pos_z")]
+        [JsonProperty("output_op_pos_z_to_op")]
         public string PosZ { get; set; }
+        [JsonProperty("output_user_pos_x_to_op")]
+        public string UserPosX { get; set; }
+        [JsonProperty("output_user_pos_y_to_op")]
+        public string UserPosY { get; set; }
+        [JsonProperty("output_user_pos_z_to_op")]
+        public string UserPosZ { get; set; }
     }
 }
